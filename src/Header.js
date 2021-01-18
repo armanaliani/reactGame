7d58ed3d9dca805be792d909aa1e14cd08ab4952
Arman Aliani <armanaliani@gmail.com>
2021-01-18 12:50:47 -0500

    logo to svg, hover effect

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import image from './assets/ticTacToeHeaderOne.png';
+import image from './assets/boardGameHeader.svg';
+
+
 
 
 const Header = () => {
     return (
         <header className="wrapper">
             <Link to="/" tabIndex="0">
-                <img className="headerImg" src={image} alt="Logo, to Home page" />
+                <img className="headerImg" src={image} alt="Logo, to Home page"/>
             </Link>
         </header>
     )
