ccd58f507af056aeb98290a96d91291ea6f689c5
Arman Aliani <armanaliani@gmail.com>
2021-01-18 15:00:06 -0500

    change header styles

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import image from './assets/boardGameHeader.svg';
 
 
 
@@ -9,7 +8,7 @@ const Header = () => {
     return (
         <header className="wrapper">
             <Link to="/" tabIndex="0">
-                <img className="headerImg" src={image} alt="Logo, to Home page"/>
+                <p>Home</p>
             </Link>
         </header>
     )
